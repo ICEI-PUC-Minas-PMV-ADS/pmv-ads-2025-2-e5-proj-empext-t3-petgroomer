d00da5e2b8c2c7c145b340834ee6f5d5df41b57f
Jonatas Coelho <jonatascoelho9@gmail.com>
2025-11-08 16:42:01 -0300

    Update next.config.js

--- codigo-fonte/front/next.config.js
+++ codigo-fonte/front/next.config.js
@@ -12,10 +12,10 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   publicRuntimeConfig: {
-    API_URL: 'https://pmv-ads-2025-2-e5-proj-empext-t3-petgroomer-production.up.railway.app/' || 'http://localhost:4000',
+    API_URL: 'https://pmv-ads-2025-2-e5-proj-empext-t3-petgroomer-production.up.railway.app' || 'http://localhost:4000',
   },
   env: {
-    NEXT_PUBLIC_API_URL: 'https://pmv-ads-2025-2-e5-proj-empext-t3-petgroomer-production.up.railway.app/' || 'http://localhost:4000'
+    NEXT_PUBLIC_API_URL: 'https://pmv-ads-2025-2-e5-proj-empext-t3-petgroomer-production.up.railway.app' || 'http://localhost:4000'
   }
 }
 
