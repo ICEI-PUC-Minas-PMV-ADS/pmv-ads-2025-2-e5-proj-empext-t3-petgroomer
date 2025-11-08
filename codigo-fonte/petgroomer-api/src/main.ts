fcccabc8dc02712f6e95ac6c4196cf34f8ffd292
Jonatas Coelho <jonatascoelho9@gmail.com>
2025-11-08 14:32:01 -0300

    fix cod

--- codigo-fonte/petgroomer-api/src/main.ts
+++ codigo-fonte/petgroomer-api/src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`API rodando em http://localhost:${port} (docs em /docs)`);
 }
 bootstrap();
